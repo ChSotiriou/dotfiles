c3ad41e261b3c326c8a4626660ee634432ceafda
Christodoulos Sotiriou <schris15@hotmail.com>
2025-10-16 21:46:31 +0300

    added dunst notifications

--- dot_config/zsh/extras/util_functions.sh
+++ dot_config/zsh/extras/util_functions.sh
@@ -1,16 +1,17 @@
 function mvd() {
     local fileName
     # check that the user is not in the Downloads directory already
-    if [[ $(pwd) == "$HOME"/Downloads ]]; then
+    DOWNLOAD_DIR="/tmp/Downloads"
+    if [[ $(pwd) == "$DOWNLOAD_DIR" ]]; then
         echo "Already in Downloads directory"
         return 1
     else
-        fileName=$(\ls -t $HOME/Downloads | head -n 1)
+        fileName=$(\ls -t "$DOWNLOAD_DIR" | head -n 1)
         # check that the command was completed successfully and has an output
         if [[ $? == 0 && -n $fileName ]]; then
             # Check if file doesn't end with .crdownload ignore casing
             if [[ ${(L)fileName} != *.crdownload ]]; then
-                mv $HOME/Downloads/"$fileName" .
+                mv "$DOWNLOAD_DIR/$fileName" .
             else
                 echo $fileName has not finished downloading
             fi
